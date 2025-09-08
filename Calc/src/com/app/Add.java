6db8eed0f17366f21e16605f6eb9f813fdb70141
nikhiltanpure <nikhiltanpure2003@gmail.com>
2025-09-08 22:15:28 +0530

    add by Nikhil

---
+++ Calc/src/com/app/Add.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Add {
+	
+	public void add()
+	{
+		int n1 =100;
+		int n2 = 50;
+		int c= n1 + n2 ;
+		System.out.println(c);
+	}
+
+}
---
+++ Calc/src/com/app/Test.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Add a = new Add();
+		a.add();
+
+	}
+
+}
